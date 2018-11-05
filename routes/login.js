b76b99fb91df67f3e1da567a01078c3fc28116cc
José Yoshiriro <jyoshiriro@gmail.com>
2018-11-05 12:51:22 -0200

    login.js

--- routes/login.js
+++ routes/login.js
@@ -19,44 +19,50 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    var username = req.body.username;
-    var password = req.body.password;
+	console.log('tentando autenticar...');
+	
+	try {
+			
+		var login = req.body.login;
+		var senha = req.body.senha;
+		
+		if (isNull(login) || isNull(senha)) {
+			console.log('Login e/ou senha inválidos');
+			res.render('login', {
+				'mensagem': 'Login e/ou senha inválidos!'
+			});
+		} else {
+			console.log('tentando consulta no banco...');
+			Database.query(`SELECT * FROM usuario WHERE login = '${login}';`, (error, results, rows) => {
+				console.log('entrou no o login');
+				const msgErro = {'mensagem': "Falha no login"};
+				if (error) {
+					res.status(401).render('login', msgErro);
+				}
+				if (results.length > 0) {
+					let decryptedPassword = cryptr.decrypt(results[0].senha);
+					if (decryptedPassword === senha) {
+						let user = {
+							nome: results[0].nome,
+							login: results[0].login,
+							id: results[0].id,
+							autenticado: true
+						};
+						req.session.user = user;
+						res.redirect('/index');
+					} else {
+						res.status(401).render('login', msgErro);
+					}
+				} else {
+					res.status(401).render('login', msgErro);
+				}
+			});
+		}
     
-    if (isNull(username) || isNull(password)) {
-        console.log('Invalid username and/or password!');
-        res.render('login', {
-            'message': 'Invalid username and/or password!'
-        });
-    } else {
-        
-        Database.query(`SELECT * FROM User WHERE user = '${username}';`, (error, results, rows) => {
-            console.log('entrou no query');
-            if (error) {
-                res.status(400).render('login', {'message': "Couldn't log in!"});
-            }
-            if (results.length > 0) {
-                let decryptedPassword = cryptr.decrypt(results[0].senha);
-                if (decryptedPassword === password) {
-                    let user = {
-                        nome: results[0].nome,
-                        user: results[0].user,
-                        id: results[0].id,
-                        logged_in: true
-                    };
-                    req.session.user = user;
-                    res.redirect('/index');
-                } else {
-                    res.render('login', {
-                        'message': 'Invalid username/password!'
-                    });
-                }
-            } else {
-                res.render('login', {
-                    'message': 'Invalid username/password!'
-                });
-            }
-        });
-    }
+	} catch (e) {
+		console.error(`erro: ${e}`);
+		res.send({'mensagem':`${e}`});
+	}
 
 });
 

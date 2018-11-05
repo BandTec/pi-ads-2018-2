baf63277de8c68ecb2d06b4eb73a1616f8d3a9c0
José Yoshiriro <jyoshiriro@gmail.com>
2018-11-05 12:56:13 -0200

    index.js

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,7 @@ router.get('/grafico.html', function(req, res, next) {
   if (!req.session.user || req.session.user && !req.session.user.logged_in) {
     res.redirect('/login.html');
   } else {
-<<<<<<< HEAD
     next();
-=======
-    res.render('index', {
-      session: req.session,
-      scripts: [
-          "index"
-      ]
-    });
->>>>>>> 7e7eb78c53e06f2facc24b377381424aa9cddd2e
   }
 });
 
